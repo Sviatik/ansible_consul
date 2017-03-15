e73c1b51e959d74841b49f29a384ff3ce6a1c281
sviat <sviat@sviat-mint18.1>
2017-03-15 03:11:48 +0200

    update

--- roles/consul/files/install_consul.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo mkdir /etc/consul.d
-sudo mkdir /var/lib/consul
-cd /tmp
-wget https://releases.hashicorp.com/consul/0.7.5/consul_0.7.5_linux_amd64.zip
-unzip consul_0.7.5_linux_amd64.zip
-sudo mv consul /usr/bin
-
